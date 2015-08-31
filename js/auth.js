187b34f8d7c889acc6d6d9f33397b84d8b6fc55a
pupudu <pubudu_pincode@live.com>
2015-08-31 16:58:29 +0530

    animations added

--- js/auth.js
+++ js/auth.js
@@ -1,12 +1,16 @@
+
 document.addEventListener('DOMContentLoaded', function() {
 
-	var btnSignin = document.getElementById('btnSignin');
-	var btnRegister = document.getElementById('btnRegister');
-	var usr = document.getElementById('usr');
-	var pwd = document.getElementById('pwd');
-  
 	try{
+		var btnSignin = document.getElementById('btnSignin');		//Sign in button in login page
+		var usr = document.getElementById('usr');	//input text field in login page
+		var pwd = document.getElementById('pwd');	//input text field in login page
+		
 		btnSignin.addEventListener('click', function() {
+			if(usr.value=="" || pwd.value ==""){
+				displayError("Please fill your details");
+				return;
+			}
 			$.post("https://id.projects.mrt.ac.lk:9000/authenticate",
 			{
 				username: usr.value,	//get value from input text field
@@ -18,15 +22,27 @@ document.addEventListener('DOMContentLoaded', function() {
 					if(data.success){
 						console.log("Authentication success");
 						window.open('main.html','_self');
+					}else{
+						displayError("Invalid Username or Password");
 					}
 				}else{
 					console.log("Error: Post request failed");
 				}
 			});
 		});
+		
+		function displayError(message){
+			$("#usr").css("border-color","red");
+			$("#pwd").css("border-color","red");
+			document.getElementById("loginError").innerText = message;
+			$("#failureMsg").fadeIn(1000);
+		}
+		
 	}catch(e){/*Do nothing*/}
 	
 	try{
+		var btnRegister = document.getElementById('btnRegister');	//Register button in login page
+		
 		btnRegister.addEventListener('click', function() {
 			chrome.tabs.getSelected(null, function(tab) {
 				chrome.tabs.executeScript(tab.id,{
