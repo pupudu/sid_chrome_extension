f17163b34273721306977eb400cb7c90df19c62d
pupudu <pubudu_pincode@live.com>
2015-08-31 09:47:01 +0530

    Register redirect to main web page

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
+
 	var btnSignin = document.getElementById('btnSignin');
+	var btnRegister = document.getElementById('btnRegister');
 	var usr = document.getElementById('usr');
 	var pwd = document.getElementById('pwd');
   
@@ -18,4 +20,17 @@ document.addEventListener('DOMContentLoaded', function() {
 			}
 		});
 	});
+	
+	btnRegister.addEventListener('click', function() {
+		chrome.tabs.getSelected(null, function(tab) {
+			chrome.tabs.executeScript(tab.id,{
+			code:'window.open("http://id.projects.mrt.ac.lk")'
+			/*file:'inject.js'*/
+			},function(){
+				console.log("navigated to");
+			});
+
+    });
+	});
+	
 }, false);
