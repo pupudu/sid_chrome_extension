2bf8dec35729547e4741aa1866957ecb78c56a93
pupudu <pubudu_pincode@live.com>
2015-08-31 12:45:56 +0530

    redirect if authenticated

--- js/auth.js
+++ js/auth.js
@@ -5,32 +5,38 @@ document.addEventListener('DOMContentLoaded', function() {
 	var usr = document.getElementById('usr');
 	var pwd = document.getElementById('pwd');
   
-	btnSignin.addEventListener('click', function() {
-		$.post("https://localhost/api/authenticate",
-		{
-			username: usr.value,
-			password: pwd.value
-		},
-		function(data, status){
-			console.log(data,status);
-			if(status=="success"){
-				window.open('main.html','_self');
-			}else{
-			
-			}
-		});
-	});
-	
-	btnRegister.addEventListener('click', function() {
-		chrome.tabs.getSelected(null, function(tab) {
-			chrome.tabs.executeScript(tab.id,{
-			code:'window.open("http://id.projects.mrt.ac.lk")'
-			/*file:'inject.js'*/
-			},function(){
-				console.log("navigated to");
+	try{
+		btnSignin.addEventListener('click', function() {
+			$.post("https://id.projects.mrt.ac.lk:9000/authenticate",
+			{
+				username: usr.value,	//get value from input text field
+				password: pwd.value		//get value from input text field
+			},
+			function(data, status){
+				console.log(data,status);
+				if(status=="success"){
+					if(data.success){
+						console.log("Authentication success");
+						window.open('main.html','_self');
+					}
+				}else{
+					console.log("Error: Post request failed");
+				}
 			});
-
-    });
-	});
+		});
+	}catch(e){/*Do nothing*/}
+	
+	try{
+		btnRegister.addEventListener('click', function() {
+			chrome.tabs.getSelected(null, function(tab) {
+				chrome.tabs.executeScript(tab.id,{
+				code:'window.open("http://id.projects.mrt.ac.lk")'
+				},function(){
+					/*Log Navigation*/
+					console.log("Redirect to Sid|Main Web Page");
+				});
+			});
+		});
+	}catch(e){/*Do nothing*/}
 	
 }, false);
