120a63f99b9a5293a8dec276a288b66b13cafc85
pupudu <pubudu_pincode@live.com>
2015-09-03 22:18:12 +0530

    Profile pic badge added

--- js/background.js
+++ js/background.js
@@ -2,8 +2,6 @@
 This script runs as a backround script. To stop, change the manifest.json
 */
 
-/*TODO create a separate script file for background tasks*/
-
 /** This will be fired whenever the current active tab changes. Runs only when this script file is active*/
 chrome.webNavigation.onCompleted.addListener(function(obj){		
 	console.log(obj);
@@ -16,4 +14,15 @@ chrome.webNavigation.onCompleted.addListener(function(obj){
 			});
 		});
 	}
+});
+
+/** Used by content scripts to access local storage*/
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.method == "verIco")
+		//var profilePic = document.getElementsByClassName("profilePicThumb")[0];
+		//var icon = document.createElement("DIV");
+		//icon.innerHTML = "<img src='resources/icons/Verified.png' width='50' height='50' style='position:absolute;left:0px;top:0px;z-index:+1;'>"
+		sendResponse({ico: "dd"});
+    else
+		sendResponse({}); // snub them.
 });
