6e3c71481c77050da5a77df6547568d5cb4431ea
pupudu <pubudu_pincode@live.com>
2015-09-12 09:19:49 +0530

    organized file structure

--- js/fbInject.js
+++ js/fbInject.js
@@ -36,7 +36,7 @@ function manipulate(){
 	
 	//node.innerHTML = '<div id = "test" class="dropdown"><button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">sid Analytics<span class="caret"></span></button><ul class="dropdown-menu"><li><a href="#">HTML</a></li><li><a href="#">CSS</a></li><li><a href="#">JavaScript</a></li></ul></div>';
 	
-	$.get(chrome.extension.getURL("test.html"), function(data) {
+	$.get(chrome.extension.getURL("html/sidAnalytics.html"), function(data) {
 		//$(data).appendTo('body');
 		// Or if you're using jQuery 1.8+:
 		// $($.parseHTML(data)).appendTo('body');
