5e52081d43be90b1c867dbcef636fba9f423f3ca
pupudu <pubudu_pincode@live.com>
2015-11-01 12:57:15 +0530

    issue with fb domain name fixed

--- js/background.js
+++ js/background.js
@@ -5,7 +5,7 @@ This script runs as a backround script. To stop, change the manifest.json
 /** This will be fired whenever the current active tab changes. Runs only when this script file is active*/
 chrome.webNavigation.onCompleted.addListener(function(obj){		
 	console.log(obj);
-	if(obj.url.search("https://www.facebook.com")===0){
+	if(obj.url.search("https://www.facebook.com")===0 || obj.url.search("https://web.facebook.com")===0){
 		chrome.tabs.getSelected(null, function(tab) {
 			chrome.tabs.executeScript(tab.id,{
 				file:'js/fbInject.js'	/*Run this script if navigated to a fb origined page*/
