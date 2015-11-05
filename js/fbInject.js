41e1997fe3a79485c4f66b5425d7509bd02cbb63
pupudu <pubudu_pincode@live.com>
2015-11-05 18:19:38 +0530

    code cleanup and improve logging

--- js/fbInject.js
+++ js/fbInject.js
@@ -13,7 +13,7 @@ if(getCookie("sidSession")==="true"){	/*check whether user is logged in*/
 
 /**identify web page and take required actions*/
 function identify(){
-	console.log("Identifying Web Page");
+	console.log(".. Identifying Web Page");
 	if(timeLineCName!=null && timeLineHLine!=null){
 		var isAbout = (document.getElementById("medley_header_about") != null);
 		if(sidId === null){
@@ -26,146 +26,16 @@ function identify(){
 				manipulateTimeLine();	/*if an fb profile timeline, and haven't modified before, then add sid elements*/
 			}
 		}else{
-			console.log("page already modified");
+			console.log(".. .. page already modified");
 		/**TODO Handle event capture issue*/
 		}
 		//addIconsToPopupMenus();
 	}
 }
 
-function manipulateAbout(){
-	console.log("updating about work page");
-	var claimAr = document.getElementsByClassName("_2lzr _50f5 _50f7");
-	var claimCount = claimAr.length; /*Number of claims on about page*/
-	if(claimCount >0){
-		setVisitStatus(0);	/*Mark about work page as visited*/
-	}
-	for(var i=0;i<claimCount;i++){
-		var claim = claimAr[i];
-		scoreClaimsOnTimeLine(i,claim,"About"); /*TODO fix issue in icon positions of about page*/
-		popUpOnIcons('claim',i,claimCount);
-	}
-}
-
-function manipulateTimeLine(){
-	console.log("updating fb time line");
-	var claimAr = document.getElementsByClassName("_1zw6 _md0 _5vb9");
-	var claimCount = claimAr.length; /*Number of claims on timeline*/
-	if(claimCount >0){
-		setVisitStatus(1);	/*Mark timeline as visited*/
-	}
-	for(var i=0;i<claimCount;i++){
-		var claim = claimAr[i].getElementsByClassName("_50f3")[0];
-		scoreClaimsOnTimeLine(i,claim,"");
-		popUpOnIcons('claim',i,claimCount);
-	}
-}
-
-function setVisitStatus(page){
-	if(sidId != null){
-		sidId.innerText = page;
-		return;
-	}
-	console.log("setting visit status: " + page);
-	sidId = document.createElement("DIV"); 
-	sidId.innerHTML = "<p id='sidId' style = 'display:none'>"+page+"</p>";
-	document.getElementsByClassName('photoContainer')[0].appendChild(sidId);
-	addSidAnalyticsMenu();
-}
-
-function addSidAnalyticsMenu(){
-	console.log("added sid analytics pop up memu");
-	//timeLineCName.innerHTML += '<span class="_5rqt"><span class="_5rqu"><span data-hover="tooltip" data-tooltip-position="right" class="_56_f _5dzy _5d-1 _5d-3" id="u_jsonp_2_7" aria-label="sID Verified User"></span></span></span>'
-	var node = document.createElement("DIV");  
-//	node.innerHTML=('<div class="_6a uiPopover _6-6 _9rx _5v-0" id="u_0_p"><a class="_9ry _p" href="#" aria-haspopup="true" aria-expanded="false" rel="toggle" role="button" id="u_0_q1" aria-owns="u_a_0">sID Analytics<i class="_bxy img sp_qk8sNUxukfD sx_1586e3"></i></a></div>');
-//	node.innerHTML=('<div class="_6a uiPopover dropdown _6-6 _9rx _5v-0" id="u_0_p1"><a class="_9ry _p" id="u_0_q1" aria-owns="u_a_0">sID Analytics</a></div>');
-//	node.innerHTML = '<ul class="_6a dropdown _6-6 _9rx _5v-0 "><li><a href="…">Page 1</a><ul><li><a href="…">Sub-page 1.1</a></li><li><a href="…">Sub-page 1.2</a></li></ul></li></ul>';
-//node.innerHTML = '<div id = "test" class="dropdown"><button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">sid Analytics<span class="caret"></span></button><ul class="dropdown-menu"><li><a href="#">HTML</a></li><li><a href="#">CSS</a></li><li><a href="#">JavaScript</a></li></ul></div>';	
-	$.get(chrome.extension.getURL("html/sidAnalytics.html"), function(data) {
-		//$(data).appendTo('body');
-		// Or if you're using jQuery 1.8+:
-		// $($.parseHTML(data)).appendTo('body');
-		//console.log(data);
-		node.innerHTML = data;
-		commitChart();
-	});
-	document.getElementsByClassName('_6_7 clearfix')[0].appendChild(node);
-}
-
-function clearSkipIcons(){
-	//clearSkipIconsUsingIcon();
-	clearSkipIconsUsingStrings();
-	clearEmptyIcons();
-}
-
-function clearSkipIconsUsingStrings(){
-	console.log("clearing icons using string comparison");
-	var itemAr = document.getElementsByClassName("_2m_3 _3-91 _8o _8s lfloat _ohe img sp_shwI5B09H5u");
-	var skipStringAr = ["Your friend since","Followed by","friends","Friends on"];
-	var nonSkipStringAr = ["Works","Lives in","From","Born on","Studies","Studied", "In a relationship"];
-	for(var i=0;i<itemAr.length;i++){
-		var text = itemAr[i].parentNode.getElementsByClassName("_50f3")[0].innerText;
-		if(text.length == 2){
-			text = itemAr[i].parentNode.getElementsByClassName("_50f3")[0].innerHTML.toString();
-		}
-		for(var j=0;j<skipStringAr.length;j++){
-			if(text.indexOf(skipStringAr[j])>=0){
-				console.log("Will clear "+ itemAr[i]+ " due to "+ skipStringAr[j]);
-				var skipClear = false;
-				for(var k=0;k<nonSkipStringAr.length;k++){
-					if(text.indexOf(nonSkipStringAr[k])>=0){
-						console.log("will not clear" + itemAr[i]+ " due to "+ nonSkipStringAr[k]);
-						skipClear = true;
-						break;
-					}
-				}
-				if(skipClear){
-					continue;
-				}
-				var parent = itemAr[i].parentNode;
-				var icon = parent.getElementsByClassName("rateIconContainer")[0];
-				if(icon != undefined){
-					icon.remove();
-				}
-			}
-		}
-	}
-}
-
-function clearSkipIconsUsingIcon(){
-	console.log("clearing icons using list icon class");
-	var skipList = ["sx_548137","sx_a8fd72","sx_2b5d8b","sx_5d6323","sx_6ec049","sx_f0a7ca"];
-	for(var i = 0; i<skipList.length; i++){
-		var itemAr = document.getElementsByClassName(skipList[i]);
-		if(itemAr.length === 0){
-			continue;
-		}
-		for(var j=0;j<itemAr.length;j++){
-			var text = itemAr[j].parentNode.getElementsByClassName("_50f3")[0].innerText;
-			if(text.indexOf("from")!=-1){
-				continue;
-			}
-			//console.log(text.indexOf("from")!=-1);
-			itemAr[j].parentNode.getElementsByClassName("rateIconContainer")[0].remove();
-		}
-	}
-}
-
-function clearEmptyIcons(){
-	console.log("clearing icons of incomplete data list items");
-	var itemAr = document.getElementsByClassName("_4bl7 _4bl8");
-	for(var i = 0;i<itemAr.length; i++){
-		if(itemAr[i].firstChild.nodeName != "BUTTON"){
-			continue;
-		}
-		var parent = itemAr[i].parentNode;
-		parent.getElementsByClassName("rateIconContainer")[0].remove();
-	}
-}
-
 /** Appends sid-rating state over fb profile picture*/
 function updateProfPic(){
-	console.log("updating profile pic");
+	console.log(".. .. updating profile pic");
 	var profPic = document.getElementsByClassName("photoContainer")[0];
 	var icon = document.createElement("DIV");
 	var imgURL;var profID = extract_UserID();
@@ -183,8 +53,69 @@ function updateProfPic(){
 	});
 }
 
+
+function manipulateAbout(){
+	console.log(".. .. updating about work page");
+	var claimAr = document.getElementsByClassName("_2lzr _50f5 _50f7");
+	var claimCount = claimAr.length; /*Number of claims on about page*/
+	if(claimCount >0){
+		setVisitStatus(0);	/*Mark about work page as visited*/
+	}
+	for(var i=0;i<claimCount;i++){
+		var claim = claimAr[i];
+		scoreClaimsOnTimeLine(i,claim,"About"); /*TODO fix issue in icon positions of about page*/
+		popUpOnIcons('claim',i,claimCount);
+	}
+}
+
+function manipulateTimeLine(){
+	console.log(".. .. updating fb time line");
+	var claimAr = document.getElementsByClassName("_1zw6 _md0 _5vb9");
+	var claimCount = claimAr.length; /*Number of claims on timeline*/
+	if(claimCount >0){
+		setVisitStatus(1);	/*Mark timeline as visited*/
+	}
+	for(var i=0;i<claimCount;i++){
+		var claim = claimAr[i].getElementsByClassName("_50f3")[0];
+		scoreClaimsOnTimeLine(i,claim,"");
+		popUpOnIcons('claim',i,claimCount);
+	}
+}
+
+function setVisitStatus(page){
+	if(sidId != null){
+		sidId.innerText = page;
+		return;
+	}
+	console.log(".. .. .. setting visit status: " + page);
+	sidId = document.createElement("DIV"); 
+	sidId.innerHTML = "<p id='sidId' style = 'display:none'>"+page+"</p>";
+	document.getElementsByClassName('photoContainer')[0].appendChild(sidId);
+	addSidAnalyticsMenu();
+}
+
+function addSidAnalyticsMenu(){
+	console.log(".. .. .. added sid analytics pop up memu");
+	//timeLineCName.innerHTML += '<span class="_5rqt"><span class="_5rqu"><span data-hover="tooltip" data-tooltip-position="right" class="_56_f _5dzy _5d-1 _5d-3" id="u_jsonp_2_7" aria-label="sID Verified User"></span></span></span>'
+	var node = document.createElement("DIV");  
+//	node.innerHTML=('<div class="_6a uiPopover _6-6 _9rx _5v-0" id="u_0_p"><a class="_9ry _p" href="#" aria-haspopup="true" aria-expanded="false" rel="toggle" role="button" id="u_0_q1" aria-owns="u_a_0">sID Analytics<i class="_bxy img sp_qk8sNUxukfD sx_1586e3"></i></a></div>');
+//	node.innerHTML=('<div class="_6a uiPopover dropdown _6-6 _9rx _5v-0" id="u_0_p1"><a class="_9ry _p" id="u_0_q1" aria-owns="u_a_0">sID Analytics</a></div>');
+//	node.innerHTML = '<ul class="_6a dropdown _6-6 _9rx _5v-0 "><li><a href="…">Page 1</a><ul><li><a href="…">Sub-page 1.1</a></li><li><a href="…">Sub-page 1.2</a></li></ul></li></ul>';
+//node.innerHTML = '<div id = "test" class="dropdown"><button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">sid Analytics<span class="caret"></span></button><ul class="dropdown-menu"><li><a href="#">HTML</a></li><li><a href="#">CSS</a></li><li><a href="#">JavaScript</a></li></ul></div>';	
+	$.get(chrome.extension.getURL("html/sidAnalytics.html"), function(data) {
+		//$(data).appendTo('body');
+		// Or if you're using jQuery 1.8+:
+		// $($.parseHTML(data)).appendTo('body');
+		//console.log(data);
+		node.innerHTML = data;
+		commitChart();
+	});
+	document.getElementsByClassName('_6_7 clearfix')[0].appendChild(node);
+}
+
+
 function scoreClaimsOnTimeLine(arrIndex, cla, classOffset){
-	console.log("scoring claims on time line");
+	console.log(".. .. scoring claims on time line");
 	var profID = extract_UserID();
 	//var cla = document.getElementsByClassName("_1zw6 _md0 _5vb9")[arrIndex].getElementsByClassName("_50f3")[0];
 	var claim = document.createElement("DIV");
@@ -231,8 +162,100 @@ function popUpOnIcons(iconClass,i,max){ //TODO
 			clearSkipIcons();
 		}
 	});
-	
 }
+
+/** Inject png images as src of icons in popup menus of claim score icons*/
+function addIconsToPopupMenus(){
+	console.log(".. .. .. injecting icons to pop up menus");
+	var verified = document.getElementsByClassName("popVerifiedIcon");
+	var neutral = document.getElementsByClassName("popNeutralIcon");
+	var refuted = document.getElementsByClassName("popRefutedIcon");
+	var popupBase = document.getElementsByClassName("popupbase");
+	
+	verImgUrl = chrome.extension.getURL("resources/icons/claimT.png");
+	neuImgUrl = chrome.extension.getURL("resources/icons/claimC.png");
+	refImgUrl = chrome.extension.getURL("resources/icons/claimR.png");
+	baseImgUrl = chrome.extension.getURL("resources/icons/popupBase.png");
+	
+	for(var i=0;i<verified.length;i++){
+		verified[i].src = verImgUrl;
+		neutral[i].src = neuImgUrl;
+		refuted[i].src = refImgUrl;
+		popupBase[i].src = baseImgUrl;
+	}
+}
+
+function clearSkipIcons(){
+	clearSkipIconsUsingStrings();
+	//clearSkipIconsUsingIcon();
+	clearEmptyIcons();
+}
+
+function clearSkipIconsUsingStrings(){
+	console.log(".. .. .. clearing icons using string comparison");
+	var itemAr = document.getElementsByClassName("_2m_3 _3-91 _8o _8s lfloat _ohe img sp_shwI5B09H5u");
+	var skipStringAr = ["Your friend since","Followed by","friends","Friends on"];
+	var nonSkipStringAr = ["Works","Lives in","From","Born on","Studies","Studied", "In a relationship"];
+	for(var i=0;i<itemAr.length;i++){
+		var text = itemAr[i].parentNode.getElementsByClassName("_50f3")[0].innerText;
+		if(text.length == 2){
+			text = itemAr[i].parentNode.getElementsByClassName("_50f3")[0].innerHTML.toString();
+		}
+		for(var j=0;j<skipStringAr.length;j++){
+			if(text.indexOf(skipStringAr[j])>=0){
+				console.log("Will clear "+ itemAr[i]+ " due to "+ skipStringAr[j]);
+				var skipClear = false;
+				for(var k=0;k<nonSkipStringAr.length;k++){
+					if(text.indexOf(nonSkipStringAr[k])>=0){
+						console.log("will not clear" + itemAr[i]+ " due to "+ nonSkipStringAr[k]);
+						skipClear = true;
+						break;
+					}
+				}
+				if(skipClear){
+					continue;
+				}
+				var parent = itemAr[i].parentNode;
+				var icon = parent.getElementsByClassName("rateIconContainer")[0];
+				if(icon != undefined){
+					icon.remove();
+				}
+			}
+		}
+	}
+}
+
+function clearSkipIconsUsingIcon(){
+	console.log(".. .. .. clearing icons using list icon class");
+	var skipList = ["sx_548137","sx_a8fd72","sx_2b5d8b","sx_5d6323","sx_6ec049","sx_f0a7ca"];
+	for(var i = 0; i<skipList.length; i++){
+		var itemAr = document.getElementsByClassName(skipList[i]);
+		if(itemAr.length === 0){
+			continue;
+		}
+		for(var j=0;j<itemAr.length;j++){
+			var text = itemAr[j].parentNode.getElementsByClassName("_50f3")[0].innerText;
+			if(text.indexOf("from")!=-1){
+				continue;
+			}
+			//console.log(text.indexOf("from")!=-1);
+			itemAr[j].parentNode.getElementsByClassName("rateIconContainer")[0].remove();
+		}
+	}
+}
+
+function clearEmptyIcons(){
+	console.log("clearing icons of incomplete data list items");
+	var itemAr = document.getElementsByClassName("_4bl7 _4bl8");
+	for(var i = 0;i<itemAr.length; i++){
+		if(itemAr[i].firstChild.nodeName != "BUTTON"){
+			continue;
+		}
+		var parent = itemAr[i].parentNode;
+		parent.getElementsByClassName("rateIconContainer")[0].remove();
+	}
+}
+
 
 /**Returns logged in user id as a string*/
 function extract_UserID(){
@@ -249,7 +272,6 @@ function extract_UserID(){
 		window.location.reload();
 	}
 	return profID;
-	
 }
 
 /**Get a cookie from main browser*/
@@ -262,6 +284,13 @@ function getCookie(cname) {
         if (c.indexOf(name) === 0) return c.substring(name.length,c.length);
     }
     return "";
+}
+
+function commitChart(){
+	var sidDropdown = document.getElementById('sidDropdown');
+	sidDropdown.addEventListener('mouseover', function() {
+		drawPieChart();
+	});
 }
 
 /*TODO Move post request to solve loading time issues*/
@@ -312,18 +341,10 @@ function drawPieChart(){
 		}catch(err){
 			console.log(err);
 		}
-		
-	});
-	
-}
-function commitChart(){
-	var sidDropdown = document.getElementById('sidDropdown');
-	sidDropdown.addEventListener('mouseover', function() {
-		drawPieChart();
 	});
 }
 
-function commitPopup(itemId){
+function commitPopup(itemId){ /* Method not in use at the moment*/
 	var item = document.getElementById(itemId);
 	item.addEventListener('mouseover', function() {
 		showRatingPopupMenu(itemId);
@@ -342,22 +363,3 @@ function overrideOverflowProperty(){
 	}
 }
 
-function addIconsToPopupMenus(){
-	console.log("injecting icons to pop up menus");
-	var verified = document.getElementsByClassName("popVerifiedIcon");
-	var neutral = document.getElementsByClassName("popNeutralIcon");
-	var refuted = document.getElementsByClassName("popRefutedIcon");
-	var popupBase = document.getElementsByClassName("popupbase");
-	
-	verImgUrl = chrome.extension.getURL("resources/icons/claimT.png");
-	neuImgUrl = chrome.extension.getURL("resources/icons/claimC.png");
-	refImgUrl = chrome.extension.getURL("resources/icons/claimR.png");
-	baseImgUrl = chrome.extension.getURL("resources/icons/popupBase.png");
-	
-	for(var i=0;i<verified.length;i++){
-		verified[i].src = verImgUrl;
-		neutral[i].src = neuImgUrl;
-		refuted[i].src = refImgUrl;
-		popupBase[i].src = baseImgUrl;
-	}
-}
