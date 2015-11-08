34017c106d719043cd6921678350544b659b4cf3
pupudu <pubudu_pincode@live.com>
2015-11-08 17:47:41 +0530

    tab change event base on tab update

--- js/background.js
+++ js/background.js
@@ -2,17 +2,17 @@
 This script runs as a backround script. To stop, change the manifest.json
 */
 
-/** This will be fired whenever the current active tab changes. Runs only when this script file is active*/
-chrome.webNavigation.onCompleted.addListener(function(obj){		
-	
-	if(obj.url.search("https://www.facebook.com")===0 || obj.url.search("https://web.facebook.com")===0){
-		chrome.tabs.getSelected(null, function(tab) {
+/** This will be fired whenever a tab changes. Runs only when this script file is active*/
+chrome.tabs.onUpdated.addListener(function (tabId,obj,tab){
+	if(obj.status === "complete"){	//inject script only after update is complete
+		alert("status "+obj.status+ " url "+ tab.url);
+		if(tab.url.search("https://www.facebook.com")===0 || tab.url.search("https://web.facebook.com")===0){
 			chrome.tabs.executeScript(tab.id,{
-				file:'js/fbInject.js'	/*Run this script if navigated to a fb origined page*/
+				file:'js/fbInject.js'	//Run this script if navigated to a fb origined page
 			},function(){
-			/*Do Nothing*/
+				//Do Nothing
 			});
-		});
+		}
 	}
 });
 
